b8c824214037a0b52f5d1476222a9f9e8d47065c
cadev-code <carlosdev1810@gmail.com>
2025-09-01 23:04:41 -0600

    feat(server): add winston helper to generate logs

---
+++ server/src/helpers/logger.ts
@@ -0,0 +1,42 @@
+import winston from 'winston';
+
+const env = process.env.NODE_ENV || 'development';
+
+const { combine, printf, timestamp, colorize } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+const transports =
+  env === 'production'
+    ? [
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error',
+        }),
+        new winston.transports.File({
+          filename: 'logs/warn.log',
+          level: 'warn',
+        }),
+        new winston.transports.File({
+          filename: 'logs/info.log',
+          level: 'info',
+        }),
+        new winston.transports.File({
+          filename: 'logs/combined.log',
+        }),
+      ]
+    : [
+        new winston.transports.Console({
+          format: combine(colorize(), logFormat),
+        }),
+      ];
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
+  transports,
+});
+
+export default logger;
