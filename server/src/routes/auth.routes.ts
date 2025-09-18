997398d82059817bc483fc60f77ebe42857fd23f
Carlos Alberto <carlosdev1810@gmail.com>
2025-09-18 13:50:11 -0600

    feat(server): improve validation of additional data to zod create user schema

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { validateInput } from '../middlewares';
 import { createUser } from '../controllers';
-import { newUserSchema } from '../schemas';
+import { createUserSchema } from '../schemas';
 
 const router = Router();
 
-router.post('/auth/create-user', validateInput(newUserSchema), createUser);
+router.post('/auth/create-user', validateInput(createUserSchema), createUser);
 
 export default router;
--- server/src/schemas/auth.ts
+++ server/src/schemas/auth.ts
@@ -1,9 +1,22 @@
 import { z } from 'zod';
 
-export const newUserSchema = z.object({
+const username = z
+  .string()
+  .min(4)
+  .regex(/^[a-z.@]+$/, 'Solo se permiten letras minúsculas, puntos y @');
+
+const password = z
+  .string()
+  .min(8)
+  .regex(/[A-Z]/, 'Debe contener al menos una letra mayúscula')
+  .regex(/[a-z]/, 'Debe contener al menos una letra minúscula')
+  .regex(/[0-9]/, 'Debe contener al menos un número')
+  .regex(/[!@#$%^&*]/, 'Debe contener al menos un carácter especial');
+
+export const createUserSchema = z.object({
   fullname: z.string().min(8),
-  username: z.string().min(4),
-  password: z.string().min(8),
+  username,
+  password,
 });
 
-export type NewUserBody = z.infer<typeof newUserSchema>;
+export type CreateUserBody = z.infer<typeof createUserSchema>;
