e476574b5d6b91d13e537c552f8b4b0ecf49e721
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-03 17:01:04 -0600

    feat(client): wrap App component with QueryClientProvider for react-query integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { App } from './App';
 import { BrowserRouter } from 'react-router';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './config/queryClient';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
