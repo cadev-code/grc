66f2ced93b9d620017dfbec67bdac88e961a962a
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-03 16:46:23 -0600

    fix(server): add cookie parser middleware to express app

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import authRoutes from './routes/auth.routes';
 import { errorHandler } from './middlewares';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = 8080;
@@ -11,6 +12,7 @@ const PORT = 8080;
 dotenv.config();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cookieParser());
 
 app.use(express.json());
 
