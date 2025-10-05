5b2bba7bd94787b69a496368ee47afe78c7ab03f
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-05 13:14:05 -0600

    feat(client): add useAuthStore for managing authentication state

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,1 +1,2 @@
 export * from './useAlertStore';
+export * from './useAuthStore';
---
+++ client/src/store/useAuthStore.ts
@@ -0,0 +1,14 @@
+import { User } from '@/types';
+import { create } from 'zustand';
+
+type AuthState = {
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  login: (user) => set({ user }),
+  logout: () => set({ user: null }),
+}));
