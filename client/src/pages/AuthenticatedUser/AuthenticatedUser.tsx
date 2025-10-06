cf237cec9390eca7e4451f568e0c910cf2799f76
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-06 10:45:51 -0600

    feat(client): implement logout functionality in AuthenticatedUser component

--- client/src/pages/AuthenticatedUser/AuthenticatedUser.tsx
+++ client/src/pages/AuthenticatedUser/AuthenticatedUser.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button';
+import { useLogout } from '@/hooks';
+
 export const AuthenticatedUser = () => {
-  return <div>AuthenticatedUser</div>;
+  const logout = useLogout();
+
+  return (
+    <div className="p-4 space-y-4">
+      <h1>Usuario autenticado</h1>
+      <Button onClick={() => logout.mutate()}>Cerrar sesión</Button>
+    </div>
+  );
 };
