a9cfa24cb8bc345e055a32033d531fcd49aabb8a
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-07 11:37:49 -0600

    feat(client): implement AppSidebar component with user management features

---
+++ client/src/components/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,80 @@
+import { ChevronUp, LogOut, ShieldCheck, User2 } from 'lucide-react';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '../ui/sidebar';
+import { Link } from 'react-router';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { useCurrentUser, useLogout } from '@/hooks';
+
+const items = [
+  {
+    title: 'Gestión de Incidentes',
+    url: '/incidentes',
+    icon: ShieldCheck,
+  },
+];
+export const AppSidebar = () => {
+  const { data: user } = useCurrentUser();
+  const logout = useLogout();
+
+  return (
+    <Sidebar className="select-none">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Gestión de Análisis y Riesgos</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url} className="flex items-center gap-1">
+                      <item.icon size={20} />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton className="cursor-pointer py-4">
+                  <User2 />
+                  <span className="truncate">{user?.fullname}</span>
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="top" align="end">
+                <DropdownMenuItem
+                  className="cursor-pointer transition-colors hover:!bg-red-50"
+                  onClick={() => logout.mutate()}
+                >
+                  <span>Cerrar Sesión</span>
+                  <LogOut className="text-red-600" />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
