037fd5e031ddfe2ad3af8fa222499b7ae15227af
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-12 14:47:02 -0600

    refactor(client): enhance useRoles hook with detailed error handling and retry logic

--- client/src/hooks/useRoles.ts
+++ client/src/hooks/useRoles.ts
@@ -4,10 +4,14 @@ import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 export const useRoles = () => {
-  return useQuery<RolesResponse, AxiosError>({
+  return useQuery<
+    RolesResponse,
+    AxiosError & { response: { data: { error: string; message: string } } }
+  >({
     queryKey: ['roles'],
     queryFn: () => fetcher<RolesResponse>('/roles/all'),
-    retry: true,
+    retry: 2, // Reintenta 2 veces en caso de error
+    retryDelay: 1000, // Espera 1 segundos entre reintentos
     staleTime: 1000 * 60, // 1 minuto en cache
   });
 };
