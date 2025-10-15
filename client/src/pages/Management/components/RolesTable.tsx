4ad0c185c8cdd5f9bbd6932051dd404a302d1f9d
Carlos Alberto <carlosdev1810@gmail.com>
2025-10-14 19:40:43 -0600

    feat(client): add RolesTable and rolesColumns components for roles management

---
+++ client/src/pages/Management/components/RolesTable.tsx
@@ -0,0 +1,64 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Rol } from '@/types';
+import {
+  ColumnDef,
+  flexRender,
+  Table as ReactTable,
+} from '@tanstack/react-table';
+
+export const RolesTable = ({
+  columns,
+  table,
+}: {
+  columns: ColumnDef<Rol>[];
+  table: ReactTable<Rol>;
+}) => {
+  return (
+    <div className="overflow-hidden rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
---
+++ client/src/pages/Management/components/rolesColumns.tsx
@@ -0,0 +1,63 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { Rol } from '@/types';
+import { Button } from '@/components/ui/button';
+import { MoreHorizontal, Pencil, Trash2 } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+type RolesColumnsProps = {
+  onEdit: (rol: Rol) => void;
+  onDelete: (rol: Rol) => void;
+};
+
+export const rolesColumns = ({
+  onEdit,
+  onDelete,
+}: RolesColumnsProps): ColumnDef<Rol>[] => [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'rol',
+    header: 'Nombre',
+  },
+  {
+    accessorKey: 'title',
+    header: 'Descripción',
+  },
+  {
+    id: 'actions',
+    header: 'Acciones',
+    cell: ({ row }) => {
+      const role = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="cursor-pointer" variant="ghost" size="sm">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => onEdit(role)}>
+              <Pencil className="mr-2 h-4 w-4" />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => onDelete(role)}
+              className="text-destructive"
+            >
+              <Trash2 className="mr-2 h-4 w-4" />
+              Eliminar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
