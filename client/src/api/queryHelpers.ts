4740fb964f2b262bb92d3cf00d1c510f64b675c3
Carlos Alberto <carlosdev1810@gmail.com>
2025-11-06 12:51:23 -0600

    fix(client): add error handling to axios request methods in queryHelpers

--- client/src/api/queryHelpers.ts
+++ client/src/api/queryHelpers.ts
@@ -1,8 +1,18 @@
 import { axiosClient } from '@/lib';
+import { AxiosError } from 'axios';
 
 export const fetcher = async <T>(url: string): Promise<T> => {
-  const { data } = await axiosClient.get<T>(url);
-  return data;
+  try {
+    const { data } = await axiosClient.get<T>(url);
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError && !error.status) {
+      console.error('Fetching error', {
+        message: error.message,
+      });
+    }
+    throw error;
+  }
 };
 
 export const poster = async <T, B = unknown>(
@@ -10,19 +20,46 @@ export const poster = async <T, B = unknown>(
   body?: B,
 ): Promise<T> => {
   // B para el tipado opcional del body
-  const { data } = await axiosClient.post<T>(url, body);
-  return data;
+  try {
+    const { data } = await axiosClient.post<T>(url, body);
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError && !error.status) {
+      console.error('Posting error', {
+        message: error.message,
+      });
+    }
+    throw error;
+  }
 };
 
 export const putter = async <T, B = unknown>(
   url: string,
   body: B,
 ): Promise<T> => {
-  const { data } = await axiosClient.put<T>(url, body);
-  return data;
+  try {
+    const { data } = await axiosClient.put<T>(url, body);
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError && !error.status) {
+      console.error('Putting error', {
+        message: error.message,
+      });
+    }
+    throw error;
+  }
 };
 
 export const deleter = async <T>(url: string): Promise<T> => {
-  const { data } = await axiosClient.delete<T>(url);
-  return data;
+  try {
+    const { data } = await axiosClient.delete<T>(url);
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError && !error.status) {
+      console.error('Deleting error', {
+        message: error.message,
+      });
+    }
+    throw error;
+  }
 };
